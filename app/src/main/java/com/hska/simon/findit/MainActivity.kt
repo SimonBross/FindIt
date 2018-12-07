b1228d66cfe523eeabad985fbb91e3d748212e8d
SimonBross <32485989+SimonBross@users.noreply.github.com>
2018-12-07 20:34:48 +0100

    Changed some IDs to proper names

--- app/src/main/java/com/hska/simon/findit/MainActivity.kt
+++ app/src/main/java/com/hska/simon/findit/MainActivity.kt
@@ -59,24 +59,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
+            R.id.nav_working_student -> {
+                // Handle the action
             }
-            R.id.nav_gallery -> {
+            R.id.nav_internship -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_thesis -> {
 
             }
-            R.id.nav_manage -> {
+            R.id.nav_settings -> {
 
             }
-            R.id.nav_share -> {
 
-            }
-            R.id.nav_send -> {
-
-            }
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
